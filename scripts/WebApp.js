4c9e88933eebfdcf097738b0b2ef497567e3a91c
Bruce McPherson <bruce@mcpher.com>
2016-08-24 10:05:41 +0100

    updated by GasGit automation

--- scripts/WebApp.js
+++ scripts/WebApp.js
@@ -1,21 +1,11 @@
-// this webapp makes your own copy of all the scripts and files used in this course.
-// input folder id 
-var ASSET_SOURCE_FOLDER_ID = "0B92ExLh4POiZVkd5Y2FVYmZmZjg";
 
-// where you want the files to be copied to
-var TARGET_FOLDER_NAME = "Gas for beginners - copy of assets";
-
-// publish this
+// v2.0.1
+// copy contents of folder
+// should be published as user accessing the app
+// more info see - http://ramblings.mcpher.com/Home/excelquirks/gassnips/copyfolders
 function doGet(e) {
 
-  /* for testing
-  e= e || {};
-  e.parameter = e.parameter || {};
-  e.parameter =  {
-    sourceid:ASSET_SOURCE_FOLDER_ID,
-    target:TARGET_FOLDER_NAME
-  };
-  */
+
   if (!e.parameter || !e.parameter.sourceid || !e.parameter.target) {
     throw 'supply parameters sourceid (the id of the source folder) and target (the path of the target folder)';
   }
